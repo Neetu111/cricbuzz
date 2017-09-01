3d9e2e7ba5fd6002ccaabf666e494bc494d3dd63
Neetu111 <201451015@gmail.com>
2017-09-01 09:41:47 +0530

    Add Documentation

--- buzz.py
+++ buzz.py
@@ -1,10 +1,20 @@
 from pycricbuzz import Cricbuzz
 import json
 import time
+#imports for connnecting with twilio
 from twilio.rest import TwilioRestClient
+
+# we import the Twilio client from the dependency we just installed
+# the following line needs your Twilio Account SID and Auth Token
+# change the "from_" number to your Twilio number and the "to" number
+# to the phone number you signed up for Twilio with, or upgrade your
+# account to send SMS to any phone number
+#pabba 9978762997
+#client.messages.create(to="+919898272433", from_="+14847274317",body=a)
 client = TwilioRestClient("AC0b85724324030f71484dd878fd6ec27f", "f700b474f7f686b2ad88ffcaa73118b6")
 
 def buzz():
+	# main logic here 
 	c = Cricbuzz()
 	matches = c.matches()
 	h=c.livescore(['4'])
@@ -15,11 +25,6 @@ def buzz():
 	l.append(h['batting']['team'])
 	l.append(h['batting']['score'][0]['overs']+"overs")
 	l.append(h['batting']['score'][0]['runs']+"/"+h['batting']['score'][0]['wickets'])
-
-	#l.append(h['bowling']['team'])
-	#l.append(h['bowling']['score'][0]['overs']+"overs")
-	#l.append(h['bowling']['score'][0]['runs']+"/"+h['bowling']['score'][0]['wickets'])
-
 	summary=" ".join(l)
 
 	batting=[]
@@ -54,11 +59,3 @@ while(True):
 	client.messages.create(to="+919978762997", from_="+14847274317",body=only[1])
 	client.messages.create(to="+919978762997", from_="+14847274317",body=only[2])	
 	time.sleep(1)	
-
-# we import the Twilio client from the dependency we just installed
-# the following line needs your Twilio Account SID and Auth Token
-# change the "from_" number to your Twilio number and the "to" number
-# to the phone number you signed up for Twilio with, or upgrade your
-# account to send SMS to any phone number
-#pabba 9978762997
-#client.messages.create(to="+919898272433", from_="+14847274317",body=a)
